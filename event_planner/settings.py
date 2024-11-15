4d83caed73f886eeb485fc03c99e0fb913f0c051
Vedant Joshi <vedantvjoshi02@gmail.com>
2024-11-15 10:55:41 +0530

    Updated settings.py with deployment configurations

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qmufv=0jea+08*px@fg%x9qz_l$zj!e&j3vlnvmd6nkv+o72j%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','event-manager-3wcc.onrender.com']
 
 
 # Application definition
